bae4445e88069a689935ba212d4144f65c08f8ff
Jacob Brunson <github@jacobbrunson.com>
2014-05-08 21:36:04 -0600

    Added ability to fade between images

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ angular-backsplash
 =================
 
 An angular directive to fill a block element with an image.
+
+Params:
+
+url - Provide the url of the image to display
+fade - true, slow, or fast
+mode - resize, stretch, or fill
--- src/backsplash.js
+++ src/backsplash.js
@@ -1,16 +1,21 @@
 angular.module("backsplash", [])
-.directive("backsplash", function () {
+.directive("backsplash", function ($timeout) {
   return {
     restrict: 'AE',
     scope: {
       mode: '@',
-      url: '@'
+      url: '@',
+      fade: '@'
     },
     link: function (scope, elem, attrs) {
-      if (scope.mode == 'fill') {
-        elem.css({overflow: "hidden"});
-      }
+      elem.css({
+        overflow: "hidden",
+        position: "relative"
+      });
       var showingImg = null;
+      var prevImg = null;
+      var z = -1;
+
       var resize = function (img) {
         if (img == null) return;
         var containerRatio = elem[0].clientWidth/elem[0].clientHeight;
@@ -41,19 +46,51 @@ angular.module("backsplash", [])
         var delta_y = (elem[0].clientHeight - img.height)/2;
         var delta_x = (elem[0].clientWidth - img.width)/2;
         angular.element(img).css({
-          position: 'relative',
+          position: 'absolute',
           top: delta_y + "px",
-          left: delta_x + "px"
+          left: delta_x + "px",
         });
-      }
+      };
+
       var placeImage = function (img) {
         console.log("Placing image", img);
+
+        var t = 0;
+        if (scope.fade.toLowerCase() == "true") {
+          t = 1;
+        } else if (scope.fade.toLowerCase() == "slow") {
+          t = 2;
+        } else if (scope.fade.toLowerCase() == "fast") {
+          t = 0.5;
+        }
+
+        angular.element(img).css({
+          opacity: 0,
+          transition: "opacity "+t+"s ease-in-out",
+          zIndex: z--
+        });
         resize(img);
-        elem.html('');
         elem.append(img);
+
+        prevImg = showingImg;
         showingImg = img;
+
+        angular.element(prevImg).css({
+          opacity:0,
+        });
+        angular.element(showingImg).css({
+          opacity:1
+        });
+        $timeout(function () {
+          if (prevImg !== null) {
+            console.log("Removing previous image");
+            angular.element(prevImg).remove();
+            prevImg = null;
+          }
+        }, ((t*1000)+5));
         scope.$emit('imageplaced');
-      }
+      };
+
       var loadImage = function (url) {
         console.log("Loading image", url);
         var img = new Image();
@@ -62,12 +99,18 @@ angular.module("backsplash", [])
             placeImage(img);
         };
         img.src = url;
-      }
+      };
 
-      scope.$watch('url', function () { loadImage(scope.url);});
+      scope.$watch('url', function () {
+        loadImage(scope.url);
+      });
+
       window.onresize = function () {
         console.log("Window was resized", showingImg);
         resize(showingImg);
+        if (prevImg !== null) {
+          resize(prevImg);
+        }
       };
     }
   };
